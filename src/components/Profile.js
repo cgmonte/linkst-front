5570b0862783feb1a5026337e1aba4ecde31d0b7
Carlos Montenegro <cgmonte@gmail.com>
2021-09-02 01:40:01 -0300

    profile de teste printa nome

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,34 +14,43 @@ export function Profile() {
 
     const history = useHistory();
 
-    let token = UserSession.getToken()
+    let full_name = UserSession.getName()
 
     function handleClick() {
-        
+
         UserSession.removeToken()
         UserSession.removeId()
         UserSession.removeName()
-        
+
         history.push("/login");
     }
 
     return (
         <ChakraProvider>
-            <Flex flexDirection="column" justifyContent="space-between">
-                <Text fontSize="2xl" paddingTop="0.4em" color="black">
-                    Profile
-                    {token}
-                </Text>
-                <Box textAlign="center">
-                    {/* <Text>{props.email} logged in!</Text> */}
-                    <Button
-                        width="full"
-                        mt={4}
-                        onClick={handleClick}>
-                        Sign out
-                    </Button>
-                </Box>
-            </Flex>
+            {!UserSession.getToken() ? (
+
+                history.push("/login")
+
+            ) : (
+                <Flex width="full" height="100vh" alignContent="center" alignItems="center" justifyContent="center">
+
+                    <Flex flexDirection="column" justifyContent="space-between">
+                        <Text fontSize="2xl" paddingTop="0.4em" color="black">
+                            Olá, {full_name}
+                        </Text>
+                        <Box textAlign="center">
+                            {/* <Text>{props.email} logged in!</Text> */}
+                            <Button
+                                width="full"
+                                mt={4}
+                                onClick={handleClick}>
+                                Sign out
+                            </Button>
+                        </Box>
+
+                    </Flex>
+                </Flex>
+            )}
         </ChakraProvider>
     );
 }
