b4637083b113ec3133e0fa0e1b49af2644463260
Carlos Montenegro <cgmonte@gmail.com>
2021-09-09 09:45:36 -0300

    wip: layout do certificado

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,12 +8,13 @@ import {
     Text,
     Box,
     Button,
-    Progress
+    Progress,
+    AspectRatio
 } from '@chakra-ui/react';
 
 import UserSession from '../components/UserSession';
 
-import { getStraeegiaData } from '../api/StrateegiaData';
+// import { getStraeegiaData } from '../api/StrateegiaData';
 
 class Profile extends React.Component {
     constructor(props) {
@@ -37,15 +38,25 @@ class Profile extends React.Component {
     }
 
     async getStData(access_token) {
-        const strateegiaData = await getStraeegiaData({ token: access_token });
-        this.setState({ number_of_projects: strateegiaData[0].stProjects.length });
-        this.setState({ number_of_missions: strateegiaData[0].stMissions.length });
-        this.setState({ number_of_maps: strateegiaData[0].stMaps.length });
-        this.setState({ number_of_divergence_points: strateegiaData[0].stDivergencePoints.length });
-        this.setState({ number_of_convergence_points: strateegiaData[0].stConvergencePoints.length });
-        this.setState({ number_of_conversation_points: strateegiaData[0].stConversationPoints.length });
-        this.setState({ number_of_replies_from_user: strateegiaData[0].userStReplies.length });
-        this.setState({ number_of_comment_replies_from_user: strateegiaData[0].userCommentReplies.length });
+        // const strateegiaData = await getStraeegiaData({ token: access_token });
+        // this.setState({ number_of_projects: strateegiaData[0].stProjects.length });
+        // this.setState({ number_of_missions: strateegiaData[0].stMissions.length });
+        // this.setState({ number_of_maps: strateegiaData[0].stMaps.length });
+        // this.setState({ number_of_divergence_points: strateegiaData[0].stDivergencePoints.length });
+        // this.setState({ number_of_convergence_points: strateegiaData[0].stConvergencePoints.length });
+        // this.setState({ number_of_conversation_points: strateegiaData[0].stConversationPoints.length });
+        // this.setState({ number_of_replies_from_user: strateegiaData[0].userStReplies.length });
+        // this.setState({ number_of_comment_replies_from_user: strateegiaData[0].userCommentReplies.length });
+
+        this.setState({ number_of_projects: 3 });
+        this.setState({ number_of_missions: 5 });
+        // this.setState({ number_of_maps: strateegiaData[0].stMaps.length });
+        this.setState({ number_of_divergence_points: 29 });
+        this.setState({ number_of_convergence_points: 1 });
+        this.setState({ number_of_conversation_points: 12 });
+        this.setState({ number_of_replies_from_user: 14 });
+        this.setState({ number_of_comment_replies_from_user: 81 });
+
         this.setState({ fetching: false });
         //     setTimeout(function () {
         //         this.setState({ fetching: false });
@@ -96,7 +107,7 @@ class Profile extends React.Component {
                                         // overflow="hidden"
                                         marginY="2em"
                                         padding="1em"
-                                        // display="inline"
+                                    // display="inline"
                                     >
                                         <Progress size="xs" isIndeterminate />
                                     </Box>
@@ -110,41 +121,69 @@ class Profile extends React.Component {
 
 
                             ) : (
-                                <div>
-                                    <Text fontSize="2xl" paddingTop="0.4em" color="black">
-                                        {this.full_name}
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_projects} projetos inovadores
-
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_missions} jornadas de inovação
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_divergence_points} pontos de divergência
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_convergence_points} pontos de convergência
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_conversation_points} pontos de conversação
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_replies_from_user} respostas para perguntas estratégicas
-                                    </Text>
-                                    <Text>
-                                        {this.state.number_of_comment_replies_from_user} comentários em respostas para perguntas estratégicas
-                                    </Text>
-                                    < Box textAlign="center">
+                                <Flex alignItems="flex-end">
+                                    <AspectRatio ratio={16 / 9} minW="70vw" maxW="80vw">
+                                        <Box backgroundImage="cert_background.png" backgroundSize="cover" position="relative">
+                                            <Box position="absolute">
+                                                <Text fontSize="xl" paddingTop="0.4em" color="white" textAlign="right">
+                                                    Certificamos que
+                                                </Text>
+                                                <Text fontSize="3xl" paddingTop="0.4em" color="white" textAlign="right">
+                                                    {this.full_name}
+                                                </Text>
+                                                <Text fontSize="x1" paddingTop="0.4em" color="white" textAlign="right">
+                                                    é um inovador strateegia.
+                                                </Text>
+                                            </Box>
+                                            <Box color="white" position="absolute" bottom="150px" right="150px" fontSize="xl">
+                                                <Text>
+                                                    {this.state.number_of_projects} projetos inovadores
+                                                </Text>
+                                                <Text>
+                                                    {this.state.number_of_missions} jornadas de inovação
+                                                </Text>
+                                                <Text>
+                                                    {this.state.number_of_divergence_points} pontos de divergência
+                                                </Text>
+                                                <Text>
+                                                    {this.state.number_of_convergence_points} pontos de convergência
+                                                </Text>
+                                                <Text>
+                                                    {this.state.number_of_conversation_points} pontos de conversação
+                                                </Text>
+                                                <Text>
+                                                    {this.state.number_of_replies_from_user} respostas para perguntas estratégicas
+                                                </Text>
+                                                <Text>
+                                                    {this.state.number_of_comment_replies_from_user} comentários em respostas para perguntas estratégicas
+                                                </Text>
+                                            </Box>
+                                        </Box>
+                                    </AspectRatio >
+                                    < Flex textAlign="center" marginLeft="1.1em" flexDirection="column">
                                         <Button
-                                            width="full"
+                                            width="15em"
+                                            mt={4}>
+                                            Adicionar ao LinkedIn
+                                        </Button>
+                                        <Button
+                                            width="15em"
+                                            mt={4}>
+                                            Postar no feed do LinkedIn
+                                        </Button>
+                                        <Button
+                                            width="15em"
+                                            mt={4}>
+                                            Baixar como arquivo
+                                        </Button>
+                                        <Button
+                                            width="15em"
                                             mt={4}
                                             onClick={this.handleClick}>
-                                            Sign out
+                                            Sair
                                         </Button>
-                                    </Box>
-                                </div>
+                                    </Flex>
+                                </Flex>
                             )
 
                             }
